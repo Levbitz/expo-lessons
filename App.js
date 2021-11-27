960015b24352327facf74f2225f9c0bbff837081
Levbitz <maxinosamx@gmail.com>
2021-11-27 16:28:03 +0530

    custom fonts

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import AppLoading from "expo-app-loading";
+import React from "react";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+import {
+  Nunito_400Regular,
+  Nunito_700Bold,
+  Nunito_900Black_Italic,
+} from "@expo-google-fonts/nunito";
+import { StyleSheet, Text, View } from "react-native";
+
+import Home from "./src/Screens/Home/Home";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+    Nunito_400Regular,
+    Nunito_700Bold,
+    Nunito_900Black_Italic,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ fontFamily: "Inter_900Black", fontSize: 40 }}>
+          Inter Black
+        </Text>
+        <Text
+          style={{
+            fontFamily: "Nunito_700Bold",
+            fontSize: 40,
+            fontWeight: "bold",
+          }}
+        >
+          Rajat
+        </Text>
+        <Text style={styles.paragraph}>mn Nunito</Text>
+        <Home />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "600",
+  },
+
+  paragraph: {
+    fontFamily: "Nunito_900Black_Italic",
+    fontWeight: "800",
   },
 });
---
+++ src/Screens/Home/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text
+        style={{
+          fontFamily: "Nunito_700Bold",
+          fontSize: 40,
+        }}
+      >
+        Rajat
+      </Text>
+
+      <Text style={{ fontFamily: "Nunito_900Black_Italic" }}>mn Nunito</Text>
+    </View>
+  );
+};
+
+export default Home;
